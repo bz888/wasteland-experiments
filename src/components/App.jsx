e958760fede68b00c707b4a10d6733146f62cab4
bz888 <ziyu.ben158@gmail.com>
2022-03-23 18:41:20 +1300

    title scrollable

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,16 @@
-import { Button, Container, Group, MantineProvider, ScrollArea, Text, Title } from '@mantine/core'
+import { Button, Code, Container, Group, MantineProvider, ScrollArea, Text, Title } from '@mantine/core'
 import React, { useState, useRef } from 'react'
-import  useMousePosition  from '../context/MousePosition'
+// import  useMousePosition  from '../context/MousePosition'
 
 
 function App() {
 	const viewport = useRef()
 	// const [count, setCount] = useState(0)
 	// const mousePosition = useMousePosition()
+	const [scrollPosition, setScrollPosition] = useState({x: 0, y: 0})
 	return (
 		<MantineProvider theme={{
+			spacing: { lg: 90 },
 			headings: {
 				fontFamily: 'Open Sans',
 				sizes: {
@@ -16,15 +18,31 @@ function App() {
 				}
 			}
 		}}>
-			<Group className="app" position='center' direction='column' spacing='xs' style={{right: '35em', paddingTop: '5em'}}>
-				<ScrollArea style={{height: '50em'}} viewportRef={viewport}>
-					<Title>TEST-1</Title>
-					<Title>TEST-1</Title>
-					<Title>TEST-1</Title>
-					<Title>TEST-1</Title>
-					{/* <Container size="lg" px="xs">{mousePosition.x} : {mousePosition.y}</Container> */}
-		 </ScrollArea>
+			<Group 
+				className="app"
+			  style={{right: '35em', paddingTop: '5em'}}
+			>
+				<ScrollArea style={{height: '50em'}} 
+					// viewportRef={viewport} 
+					onScrollPositionChange={setScrollPosition} 
+					type='scroll'
+				>
+					<Group spacing={80} direction='column' >
+						<Title>TEST-1</Title>
+						<Title>TEST-1</Title>
+						<Title>TEST-1</Title>
+						<Title>TEST-1</Title>
+						<Title>TEST-1</Title>
+						<Title>TEST-1</Title>
+						<Title>TEST-1</Title>
+						<Title>TEST-1</Title>
+					</Group>
+		 		</ScrollArea>
+				{/* <Container size="lg" px="xs">{mousePosition.x} : {mousePosition.y}</Container> */}
 			</Group>
+		 <Text>
+				<Code>{`{ x: ${scrollPosition.x}, y: ${scrollPosition.y}}`}</Code>
+		 </Text>
 		</MantineProvider>
 	)
 }
